15b7055252c7834b1b5955be7eefa5f1a2ec9ccf
meerian <myronneozx@gmail.com>
2024-04-24 14:01:27 +0800

    Added svgs and techstack section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,20 +32,24 @@ throw new Error('Method not implemented.');
   {
     name: "PSAccess",
     description: "A web application made for the hackathon CodeSprint 2022. Built using Next.js, PostgreSQL, Prisma and React.",
-    link: "https://github.com/meerian/psa-codesprint-2022"
+    link: "https://github.com/meerian/psa-codesprint-2022",
+    img: "/assets/psaccess.svg"
   },
   {
-    name: "PSAccess",
-    description: "A web application made for the hackathon CodeSprint 2022. Built using Next.js, PostgreSQL, Prisma and React.",
-    link: "https://github.com/meerian/psa-codesprint-2022"
+    name: "Itch.io Page",
+    description: "My main Itch.io page where I upload the games I have created using Unity, Javascript and GameMaker Studios.",
+    link: "https://meerian.itch.io/",
+    img: "/assets/itchio.svg"
   },  {
-    name: "PSAccess",
-    description: "A web application made for the hackathon CodeSprint 2022. Built using Next.js, PostgreSQL, Prisma and React.",
-    link: "https://github.com/meerian/psa-codesprint-2022"
+    name: "Meerian.vip",
+    description: "Source code for this website. Built using Angular, PrimeNG and Typescript.",
+    link: "https://github.com/meerian/meerian.github.io",
+    img: "/assets/website.svg"
   },  {
-    name: "PSAccess",
-    description: "A web application made for the hackathon CodeSprint 2022. Built using Next.js, PostgreSQL, Prisma and React.",
-    link: "https://github.com/meerian/psa-codesprint-2022"
+    name: "Github Page",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    link: "https://github.com/meerian",
+    img: "/assets/psaccess.svg"
   },
 
   ];
