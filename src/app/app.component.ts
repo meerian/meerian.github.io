e3d77458adddf28f855463eb1a8e06bb36199390
meerian <myronneozx@gmail.com>
2024-04-26 14:06:08 +0800

    Completed techstack portion and refactored some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterOutlet } from '@angular/router';
 import { CarouselModule } from 'primeng/carousel';
 import { TagModule } from 'primeng/tag';
 import { ButtonModule } from 'primeng/button';
+import { projectData } from '../constants/projectData';
+import { techstackData } from '../constants/techstackData';
 
 // @NgModule({
 //   declarations: [
@@ -29,33 +31,12 @@ getSeverity(arg0: any) {
 throw new Error('Method not implemented.');
 }
   title = 'my-app';
-  projects: any = [
-  {
-    name: "PSAccess",
-    description: "A web application made for the hackathon CodeSprint 2022. Built using Next.js, PostgreSQL, Prisma and React.",
-    link: "https://github.com/meerian/psa-codesprint-2022",
-    img: "/assets/psaccess.svg"
-  },
-  {
-    name: "Itch.io Page",
-    description: "My main Itch.io page where I upload the games I have created using Unity, Javascript and GameMaker Studios.",
-    link: "https://meerian.itch.io/",
-    img: "/assets/itchio.svg"
-  },  {
-    name: "Meerian.vip",
-    description: "Source code for this website. Built using Angular, PrimeNG and Typescript.",
-    link: "https://github.com/meerian/meerian.github.io",
-    img: "/assets/website.svg"
-  },  {
-    name: "SIASA",
-    description: "SIASA is a desktop application that enables student financial advisors to quickly and easily keep track of their contacts and financial plans sold. Built using Java.",
-    link: "https://github.com/meerian",
-  },
+  projects = projectData;
 
-  ];
+  techstacks = techstackData;
 
-  getAngularLink() {
-    window.open("https://angular.io/");
+  openLink(link: string) {
+    window.open(link);
   }
 
 
---
+++ src/constants/techstackData.ts
@@ -0,0 +1,77 @@
+export const techstackData: any = [
+    {
+      name: "Angular",
+      link: "https://angular.io/",
+      img: "/assets/angular.svg"
+    },
+    {
+      name: "Java",
+      link: "https://www.java.com/en/",
+      img: "/assets/java.svg"
+    },    
+    {
+      name: "C#",
+      link: "https://dotnet.microsoft.com/en-us/languages/csharp",
+      img: "/assets/csharp.svg"
+    }, 
+    {
+      name: "Python",
+      link: "https://www.python.org/",
+      img: "/assets/python.svg"
+    }, 
+    {
+      name: "TypeScript",
+      link: "https://www.typescriptlang.org/",
+      img: "/assets/typescript.svg"
+    }, 
+    {
+      name: "JavaScript",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      img: "/assets/javascript.svg"
+    }, 
+    {
+      name: "C++",
+      link: "https://cplusplus.com/",
+      img: "/assets/cpp.svg"
+    }, 
+    {
+      name: "Unity",
+      link: "https://unity.com/",
+      img: "/assets/unity.svg"
+    }, 
+    {
+      name: "HTML 5",
+      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+      img: "/assets/html.svg"
+    }, 
+    {
+      name: ".NET",
+      link: "https://dotnet.microsoft.com/en-us/",
+      img: "/assets/dotnet.svg"
+    }, 
+    {
+      name: "Oracle DB",
+      link: "https://www.oracle.com/database/",
+      img: "/assets/oracle.svg"
+    }, 
+    {
+      name: "Node JS",
+      link: "https://nodejs.org/en",
+      img: "/assets/nodejs.svg"
+    }, 
+    {
+      name: "Git",
+      link: "https://git-scm.com/",
+      img: "/assets/git.svg"
+    }, 
+    {
+      name: "Azure",
+      link: "https://azure.microsoft.com/en-us/products/devops",
+      img: "/assets/azure.svg"
+    }, 
+    {
+      name: "Jira",
+      link: "https://www.atlassian.com/software/jira",
+      img: "/assets/jira.svg"
+    }, 
+  ]
