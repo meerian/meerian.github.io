0aa76f68a6605c8b547cb23b0471b6823576aa8e
meerian <myronneozx@gmail.com>
2024-05-01 22:30:40 +0800

    Added scalability for different screen sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,18 +17,36 @@ import { gamesData } from '../constants/gamesData';
 })
 
 export class AppComponent {
-getSeverity(arg0: any) {
-throw new Error('Method not implemented.');
-}
+  getSeverity(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
+
   title = 'my-app';
   projects = projectData;
   techstacks = techstackData;
   games = gamesData;
+  mobile = false;
+  smallscreen = false;
+
+  ngOnInit() {
+    console.log(window.screen.width)
+    if (window.screen.width <= 720) {
+      this.mobile = true;
+    } else if (window.screen.width <= 2000) {
+      this.smallscreen = true;
+    }
+  }
 
   openLink(link: string) {
     window.open(link);
   }
 
 
-responsiveOptions: any;
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1
+    }
+  ]
 }
